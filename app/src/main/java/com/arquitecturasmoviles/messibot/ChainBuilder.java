4dc060d644ec68a3186dc5ee00daa7672928f3d0
Martin Sanchez <martinsanchez@MacBook-Pro-de-Martin.local>
2018-05-08 18:33:36 -0300

    make chain

--- app/src/main/java/com/arquitecturasmoviles/messibot/ChainBuilder.java
+++ app/src/main/java/com/arquitecturasmoviles/messibot/ChainBuilder.java
@@ -1,8 +1,22 @@
 package com.arquitecturasmoviles.messibot;
 
 public class ChainBuilder {
-    public byte[] makeChain(int frameType, int data)
-    {
 
+    public static final int START_CHAIN = 0x7E;
+
+    public byte[] makeChain(int frameType, byte[] data) {
+        byte lbs = 0x0; //TODO: LBS function
+        byte mbs = 0x0; //TODO: MBS function
+
+        byte[] chain = {START_CHAIN, lbs, mbs };
+
+        for (int i = 0; i <= data.length; i++) {
+            chain[chain.length + 1] = data[i];
+        }
+
+        int checkSum = 0xFF; //TODO: function check sum
+        chain[chain.length + 1] = (byte) checkSum;
+
+        return chain;
     }
 }
