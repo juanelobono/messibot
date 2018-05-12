98f0ebe0d1648265ca60f076b1513e15e38eb4fc
mbeltramone <maatisk@gmail.com>
2018-05-11 21:20:41 -0300

    Fix check

--- app/src/main/java/com/arquitecturasmoviles/messibot/CheckSum.java
+++ app/src/main/java/com/arquitecturasmoviles/messibot/CheckSum.java
@@ -1,23 +1,18 @@
 package com.arquitecturasmoviles.messibot;
 
 public class CheckSum {
-    int CalculateCheckSum( byte[] bytes ){
+    int CalculateCheckSum(byte[] bytes) {
         int CheckSum = 0, i = 0;
 
         for( i = 0; i < bytes.length; i++){
-            CheckSum += (bytes[i] & 0xFF);
-        }
-
-        return 255 - CheckSum;
-    }
-
-    String CalculateCheckSum( Integer[] bytes ){
-        Integer CheckSum = 0, i = 0;
-
-        for( i = 0; i < bytes.length; i++ ){
             CheckSum += bytes[i];
         }
 
-        return Integer.toHexString(CheckSum);
+        if (CheckSum > 255) {
+            CheckSum = 255 - (CheckSum & 0xFF);
+            return CheckSum;
+        }
+
+        return 255 - CheckSum;
     }
 }
