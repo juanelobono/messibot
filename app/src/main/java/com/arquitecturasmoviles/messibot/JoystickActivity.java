30151d89d3bb3ea04105b61eefd3246cedbb592a
mbeltramone <maatisk@gmail.com>
2018-06-07 18:48:34 -0300

    Fix MainActivity

--- app/src/main/java/com/arquitecturasmoviles/messibot/JoystickActivity.java
+++ app/src/main/java/com/arquitecturasmoviles/messibot/JoystickActivity.java
@@ -2,39 +2,18 @@ package com.arquitecturasmoviles.messibot;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
-
 import io.github.controlwear.virtual.joystick.android.JoystickView;
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
-import java.lang.reflect.Method;
-import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
-import io.github.controlwear.virtual.joystick.android.JoystickView;
-import static android.content.ContentValues.TAG;
 
 public class JoystickActivity extends Activity {
 
-    private final int MAX_STRENGTH = 255;
-    private final int MIN_STRENGTH = 0;
-    private static final UUID MY_UUID_INSECURE =
-            UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
     private enum DIRECTION {
         LEFT,
         RIGHT,
@@ -48,16 +27,16 @@ public class JoystickActivity extends Activity {
     }
 
     BluetoothAdapter mBluetoothAdapter;
-    BluetoothConnectionService mBluetoothConnection;
-    BluetoothSocket socket;
     BluetoothDevice mBTDevice;
     private TextView tvDirection;
     private TextView tvAngle;
     private TextView tvStrength;
     private SendDataService sendDataService;
     private ChainBuilder chainBuilder;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -70,13 +49,6 @@ public class JoystickActivity extends Activity {
         tvDirection = findViewById(R.id.tvDirection);
         tvAngle = findViewById(R.id.tvAngle);
         tvStrength = findViewById(R.id.tvStrength);
-
-        /*
-         * final byte[] tramaAdelante = new byte[]{(byte)0x7e, (byte)0x00,(byte)0x02, (byte)0x04, (byte)0xFB, (byte)0x00};
-         * final byte[] tramaAtras = new byte[]{(byte)0x7e, (byte)0x00,(byte)0x02, (byte)0x01, (byte)0xFE, (byte)0x00};
-         * final byte[] tramaIzquieda = new byte[]{(byte)0x7e, (byte)0x00, (byte)0x02,(byte)0x02, (byte)0xFD, (byte)0x00};
-         * final byte[] tramaDerecha = new byte[]{(byte)0x7e, (byte)0x00,(byte)0x02, (byte)0x03, (byte)0xFC, (byte)0x00};
-         */
 
         mBTDevice = getIntent().getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 
@@ -214,7 +186,7 @@ public class JoystickActivity extends Activity {
                         tvStrength.setText(getStrengthToDecimal(strength) + " en decimal.");
                     }
                 },
-                150
+                500
         );
     }
 
@@ -242,6 +214,7 @@ public class JoystickActivity extends Activity {
 
     private int getStrengthToDecimal(int strength)
     {
+        int MAX_STRENGTH = 255;
         return (int) Math.ceil((strength * MAX_STRENGTH) / 100);
     }
 
@@ -249,9 +222,9 @@ public class JoystickActivity extends Activity {
     {
         DIRECTION direction = null;
 
-        if (angle >= 0 && angle <= 360 && strength == 0) {
-            direction = DIRECTION.CENTER;
-        }
+//        if (angle >= 0 && angle <= 360 && strength == 0) {
+//            direction = DIRECTION.CENTER;
+//        }
 
         if ((angle >= 0 && angle <= 30) || angle >= 330) {
             direction = DIRECTION.RIGHT;
@@ -272,48 +245,5 @@ public class JoystickActivity extends Activity {
         }
 
         return direction;
-    }
-
-    /*
-     *  Esta clase está destinada a utilizarse de cronómetro para la terminación automática y asíncrona
-     *  de conexiones bluetooth dados los milisegundos requeridos para que esta tarea se lleve a cabo.
-     */
-    public final class Reminder {
-        Timer timer;
-
-        Reminder(int milliseconds) {
-            timer = new Timer();
-            timer.schedule(new RemindTask(), milliseconds);
-        }
-
-        class RemindTask extends TimerTask {
-            public void run() {
-
-                Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-                if (pairedDevices.size() > 0) {
-                    for (BluetoothDevice device : pairedDevices) {
-                        try {
-                            Method m = device.getClass()
-                                    .getMethod("removeBond", (Class[]) null);
-                            m.invoke(device, (Object[]) null);
-                            Log.d(TAG, "Conexiones terminadas.");
-                        } catch (Exception e) {
-                            Log.e("Error terminación.", e.getMessage());
-                        }
-                    }
-                }
-
-                timer.cancel();
-            }
-        }
-    }
-
-    /*
-     *  Este método permite desvincular las conexiones bluetooth pasada cierta cantidad de milisegundos
-     */
-    protected void terminateBluetoothConnection(int milliseconds)
-    {
-        Reminder reminder = new Reminder(milliseconds);
-        Toast.makeText(JoystickActivity.this, "Se desvincularán todos los dispositivos en " + milliseconds / 1000 + " segundos.", Toast.LENGTH_SHORT).show();
     }
 }
